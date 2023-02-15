e8790f0219917b3f0decddf172be12dc8dadf784
mirito333 <miritom3810@outlook.jp>
2023-02-15 14:48:04 +0900

    検索用APIも追加

--- api/handler/search.go
+++ api/handler/search.go
@@ -8,15 +8,23 @@ import (
 
 func GetTrack(c *gin.Context) {
 	time := c.Query("time")
+	minTime := c.DefaultQuery("min_time", "0")
+	maxTime := c.DefaultQuery("max_time", "100000000")
+	category := c.DefaultQuery("category", "no")
 	db := connection.GetDB()
-	result := db.Where("duration = ?", time).First(&model.Track{})
-	c.JSON(200, result)
-}
-
-func GetTrackBetween(c *gin.Context) {
-	minTime := c.Query("min_time")
-	maxTime := c.Query("max_time")
-	db := connection.GetDB()
-	result := db.Where("duration between ? and ?", minTime, maxTime).First(&model.Track{})
+	relation := db
+	if time != "" {
+		relation = relation.Where("`duration` = ?", time)
+	}
+	if minTime != "0" || maxTime != "100000000" {
+		relation = relation.Where("`duration` between ? and ?", minTime, maxTime)
+	}
+	if category != "no" {
+		genre := model.Genre{}
+		db.Where("`genre` = ?" ,category).First(&genre)
+		relation = relation.Table("tracks").Joins("join track_artists on `track_artists`.`track_id` = `tracks`.`id`")
+		relation = relation.Joins("join artist_genres on `track_artists`.`artist_id` = `artist_genres`.`artist_id`").Where("`artist_genres`.`genre_id` = ?" , genre.ID)
+	}
+	result := relation.Order("times desc").First(&model.Track{})
 	c.JSON(200, result)
 }
