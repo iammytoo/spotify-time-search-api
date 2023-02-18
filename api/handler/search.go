4c74e77204134d0c4289a1501132711e387f30bc
miyamoto <miritoM3810@outlook.jp>
2023-02-18 13:12:05 +0900

    category を orに

--- api/handler/search.go
+++ api/handler/search.go
@@ -11,7 +11,7 @@ func GetTrack(c *gin.Context) {
 	time := c.Query("time")
 	minTime := c.DefaultQuery("min_time", "0")
 	maxTime := c.DefaultQuery("max_time", "100000000")
-	category := c.DefaultQuery("category", "no")
+	category := c.QueryArray("category")
 	around := c.Query("around")
 	db := connection.GetDB()
 	relation := db
@@ -21,11 +21,15 @@ func GetTrack(c *gin.Context) {
 	if minTime != "0" || maxTime != "100000000" {
 		relation = relation.Where("`duration` between ? and ?", minTime, maxTime)
 	}
-	if category != "no" {
-		genre := model.Genre{}
-		db.Where("`genre` = ?", category).First(&genre)
+	if len(category) != 0 {
+		genreIDs := []uint{}
+		genres := []model.Genre{}
+		db.Where("`genre` IN (?)", category).Find(&genres)
+		for _, g := range genres {
+			genreIDs = append(genreIDs, g.ID)
+		}
 		relation = relation.Table("tracks").Joins("join track_artists on `track_artists`.`track_id` = `tracks`.`id`")
-		relation = relation.Joins("join artist_genres on `track_artists`.`artist_id` = `artist_genres`.`artist_id`").Where("`artist_genres`.`genre_id` = ?", genre.ID)
+		relation = relation.Joins("join artist_genres on `track_artists`.`artist_id` = `artist_genres`.`artist_id`").Where("`artist_genres`.`genre_id` IN (?)", genreIDs)
 	}
 	if around != "" {
 		relation = relation.Order(gorm.Expr("abs(`duration` - ?) asc", around)).Order("times desc")
